5a8aef4f9df7b11dea6b587de52f4b3a28a9016c
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-11-04 09:36:17 +0000

    daySeven part two

--- daySeven.ts
+++ daySeven.ts
@@ -1,7 +1,6 @@
 import { readFileSync } from "fs";
 
 
-
 const median = (nums : number[]) => {
     nums.sort((a,b) => a - b);
     const mid = Math.floor(nums.length / 2);
@@ -11,13 +10,30 @@ const median = (nums : number[]) => {
     return nums[mid];
 }
 
-const solve = () => {
-    const input = readFileSync("./inputData/daySeven.txt", {"encoding": "utf-8"}).split(",").map(Number);
-    const pos = median(input);
-    let result = 0;
-    input.forEach((v, _) => {
-        result += Math.abs(v - pos);
-    })
+const stepsCost = (n: number) => {
+    return n + Math.round((n * (n-1)) / 2)
+}
+
+const solve = (input: number[]) => {
+    //const pos = median(input);
+    const min = input.reduce( (a,b) => a < b ? a : b);
+    const max = input.reduce( (a,b) => a > b ? a : b);
+    let result = 1e+9;
+    for (let pos= min; pos  <= max; pos++){
+        let currResult = 0;
+        input.forEach((v, _) => {
+            const n = Math.abs(v - pos);
+            currResult += stepsCost(n)
+        })
+        result = Math.min(result, currResult);
+    }    
     console.log(result)
 }
-solve();
+
+const input = readFileSync("./inputData/daySeven.txt", {"encoding": "utf-8"}).split(",").map(Number);
+solve(input);
+
+const ex = [16,1,2,0,4,2,7,1,2,14]
+solve(ex)
+//100220548 -> high
+//100220525 -> correct
